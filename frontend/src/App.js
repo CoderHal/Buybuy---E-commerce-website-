1e0cbaf47e16d8f018b9a3fe72670bbd5ebb002d
CoderHal <youhao1997@outlook.com>
2022-03-13 21:08:30 -0400

    collect the Rout of "ProductScreen" and " Home Screen", but still have some problems on id. Adjust the style of ProductScreen pages and add a back function on the ProductScreen pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import {Routes} from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
@@ -16,8 +17,10 @@ function App() {
               </div>
           </header>  
           <main>
-            <Route path="/product/:id" component={ProductScreen}></Route>
-            <Route path="/"  component={HomeScreen} exact></Route>
+          <Routes>
+            <Route path="/product/:id" element={<ProductScreen/>} exact></Route>
+            <Route path="/"  element={<HomeScreen/>} exact></Route>
+          </Routes>
           </main>
           <footer className="row center">All right reserved</footer>
       </div>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,55 +2,66 @@ import React from 'react';
 import Product from '../components/Product';
 import Rating from '../components/Rating';
 import data from '../data';
+import { Link, useParams } from 'react-router-dom';
 
 export default function ProductScreen(props){
-    const product= data.products.find(x => x._id === PaymentResponse.match.params.id)
-    if(!product){
+    const params=useParams();
+    const productId= data.products.find((x) => x._id );
+    if(!productId){
         return <div> Product Not Found</div>;
     }
     return(
     <div>
-        <div className="row">
+        <Link to="/">Back to results</Link>
+        <div className="row top">
             <div className="col-2">
-                <img src={product.image} alt={product.name}></img>
+                <img className="large" src={productId.image} alt={productId.name}></img>
             </div>
             <div className="col-1">
                 <ul>
                     <li>
-                        <h1>{product.name}</h1>
+                        <h1>{productId.name}</h1>
                     </li>
                     <li>
                         <Rating
-                            rating={product.rating}
-                            numReviews={product.numReviews}
+                            rating={productId.rating}
+                            numReviews={productId.numReviews}
                         ></Rating>
                     </li>
                     <li>
-                        Price: ${product.price}
+                        Price: ${productId.price}
                     </li>
                     <li>
                         Description:
-                        <p>{product.description}</p>
+                        <p>{productId.description}</p>
                     </li>
                 </ul>
             </div>
             <div className="col-1">
-                <ul>
-                    <li>
-                        <div className="row">
-                            <div>Status</div>
-                            <div>
-                                {product.countInStock>0?(
-                                    <span className="success">In Stock</span>):(
-                                    <span className="error">Unavailable</span>
-                                 )}
+                <div className="card card-body">
+                    <ul>
+                        <li>
+                            <div className="row">
+                                <div>Price</div>
+                                <div className="price">${productId.price}</div>
                             </div>
-                        </div>
-                    </li>
-                    <li>
-                        <button className="primary block">Add to Cart</button>
-                    </li>
-                </ul>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div>Status</div>
+                                <div>
+                                    {productId.countInStock>0?(
+                                        <span className="success">In Stock</span>):(
+                                        <span className="error">Unavailable</span>
+                                    )}
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <button className="primary block">Add to Cart</button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
