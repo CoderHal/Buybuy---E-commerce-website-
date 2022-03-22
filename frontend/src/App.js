4a2fd846a29566cab699b9e2a072e5b2a2221752
CoderHal <youhao1997@outlook.com>
2022-03-21 20:08:58 -0400

    add Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Navbar';
+import { LinkContainer} from 'react-router-bootstrap';
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="grid-container">
-          <header className="row">
-              <div>
-                  <Link className="brand" to="/">amazona</Link>
-              </div>
-          </header>  
-          <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen/>} exact></Route>
-            <Route path="/"  element={<HomeScreen/>} exact></Route>
-          </Routes>
-          </main>
+      <div className='d-flex flex-column site-container'>
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} exact></Route>
+              <Route path="/" element={<HomeScreen />} exact></Route>
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
