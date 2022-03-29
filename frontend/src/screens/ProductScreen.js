943ebf55f475a706ae77ba3b83df7313698a400a
CoderHal <youhao1997@outlook.com>
2022-03-28 20:17:45 -0400

    14. Implement Add to Cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect, useReducer } from 'react';
+import { useContext,useEffect, useReducer } from 'react';
 import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -13,6 +13,7 @@ import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
+import {Store} from '../Store';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -50,6 +51,14 @@ export default function ProductScreen() {
         };
         fetchData();
     }, [slug]);
+
+    const {state, dispatch: ctxDispatch} = useContext(Store);
+    const addToCartHandler = () =>{
+        ctxDispatch({
+            type:'CART_ADD_ITEM', 
+            payload: {...product, quantity: 1},
+        });
+    };
 
     return loading ? (
         <LoadingBox/>
@@ -111,7 +120,7 @@ export default function ProductScreen() {
                                     {product.countInStock > 0 && (
                                         <ListGroup.Item>
                                             <div className="d-grid">
-                                                <Button variant="primary">
+                                                <Button onClick={addToCartHandler} variant="primary">
                                                     Add to Cart
                                                 </Button>
                                             </div>
