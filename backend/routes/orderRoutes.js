44bac95faeb7fa77078600521705c6ce09390760
CoderHal <youhao1997@outlook.com>
2022-08-28 16:42:54 -0400

    Order History and Userprofile

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -26,6 +26,15 @@ orderRouter.post(
 );
 
 orderRouter.get(
+  '/mine',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
+
+orderRouter.get(
     '/:id',
     isAuth,
     expressAsyncHandler(async (req, res) => {
@@ -61,5 +70,5 @@ orderRouter.get(
       }
     })
   );
-  
+
 export default orderRouter;
